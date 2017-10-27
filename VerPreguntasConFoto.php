a1d2b496ea82a5fc73624f939d07a040e9e2bfb3
Xabier Echezurieta <xabier3ee@gmail.com>
2017-10-27 11:43:58 +0200

    mod verpreguntaconfoto

--- VerPreguntasConFoto.php
+++ VerPreguntasConFoto.php
@@ -1,9 +1,35 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Ver Preguntas</title>
-	</head>
-	<body>
+  <head>
+    <meta name="tipo_contenido" content="text/html;" http-equiv="content-type" charset="utf-8">
+	<title>Preguntas</title>
+    <link rel='stylesheet' type='text/css' href='estilos/style.css' />
+	<link rel='stylesheet' 
+		   type='text/css' 
+		   media='only screen and (min-width: 530px) and (min-device-width: 481px)'
+		   href='estilos/wide.css' />
+	<link rel='stylesheet' 
+		   type='text/css' 
+		   media='only screen and (max-width: 480px)'
+		   href='estilos/smartphone.css' />
+  </head>
+  <body>
+  <div id='page-wrap'>
+	<header class='main' id='h1'>
+		<span class="right"><a href="registro">Registrarse</a></span>
+      		<span class="right"><a href="login">Login</a></span>
+      		<span class="right" style="display:none;"><a href="/logout">Logout</a></span>
+		<h2>Quiz: el juego de las preguntas</h2>
+    </header>
+	<nav class='main' id='n1' role='navigation'>
+		<span>Inicio</spam>
+		<span><a href='quizes'>Preguntas</a></spam>
+		<span><a href='creditos.html'>Creditos</a></spam>
+		<span><a href='pregunta.html'>Añadir Pregunta</a></spam>
+	</nav>
+    <section class="main" id="s1">
+    
+	<div>
 		<?php
 			$link= mysqli_connect("localhost", "root", "", "quiz");
 			$preguntas = mysqli_query($link, "SELECT * FROM preguntas" );
@@ -15,6 +41,14 @@
 			echo '</table>';
 			$preguntas->close();
 			mysqli_close($link);
-		?>
+		?>	
+
+	</div>
+    </section>
+	<footer class='main' id='f1'>
+		<p><a href="http://es.wikipedia.org/wiki/Quiz" target="_blank">Que es un Quiz?</a></p>
+		<a href='https://github.com'>Link GITHUB</a>
+	</footer>
+</div>
 </body>
 </html>
